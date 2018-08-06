e08528d0c0f2e1f8afa754c58b69744a499b69a0
karpawar <karpawar@gmail.com>
2018-08-06 15:57:18 +0530

    Dividing Theme Structure

---
+++ src/app/theme/settingsbar/settingsbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settingsbar',
+  templateUrl: './settingsbar.component.html',
+  styleUrls: ['./settingsbar.component.css']
+})
+export class SettingsbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/theme/theme.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SettingsbarComponent } from './settingsbar/settingsbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [HeaderComponent, SidebarComponent, SettingsbarComponent, FooterComponent],
+  declarations: [HeaderComponent, SidebarComponent, SettingsbarComponent, FooterComponent]
+})
+export class ThemeModule { }
